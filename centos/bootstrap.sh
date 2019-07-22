2c850eab3367fbc252422debf9d774e37c42c251
George <knightflower1989@gmail.com>
2019-07-22 11:33:38 +0800

    Update bootstrap.sh
    update

--- centos/bootstrap.sh
+++ centos/bootstrap.sh
@@ -7,10 +7,10 @@
 yum -y install wget
 cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak  
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo  
+wget -P /etc/yum.repos.d/ http://mirrors.aliyun.com/repo/epel-7.repo 
 
 yum clean all
 yum makecache
-yum update
 
 # install build tools
 
